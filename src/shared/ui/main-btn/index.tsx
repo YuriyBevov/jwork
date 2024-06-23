59552cc36ea53d20f3c3b0d5278f1e9fb6037692
Yuriy Bevov <yuriybevov@gmail.com>
2024-06-23 17:27:27 +0300

    add service template

---
+++ src/shared/ui/main-btn/index.tsx
@@ -0,0 +1,36 @@
+import clsx from 'clsx';
+import React from 'react';
+
+import styles from './main-btn.module.scss';
+
+type Props = {
+  outlined?: boolean;
+  bordered?: boolean;
+  rounded?: 'xs' | 'lg';
+  text?: string;
+};
+
+export const MainBtn: React.FC<Props> = ({
+  outlined,
+  rounded,
+  bordered,
+  text = 'Подробнее',
+}) => {
+  console.log(outlined, rounded);
+  return (
+    <button
+      className={clsx(
+        outlined ? styles.outlined : null,
+        bordered ? styles.bordered : null,
+        rounded === 'xs'
+          ? styles.rounded_xs
+          : rounded === 'lg'
+            ? styles.rounded_lg
+            : null,
+        styles.root,
+      )}
+    >
+      {text}
+    </button>
+  );
+};
