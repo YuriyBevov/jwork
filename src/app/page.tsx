c6c7cdd59daf5bf80a96149dd0c81696f4ba063c
Yuriy Bevov <yuriybevov@gmail.com>
2024-06-25 17:08:07 +0300

    add sixth ыукмшсу еуьздфеу

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,7 @@ import { ServicesTypeOne } from '@/components/services/services-type-1';
 import { ServicesTypeTwo } from '@/components/services/services-type-2';
 import { ServicesTypeTres } from '@/components/services/services-type-3';
 import { ServicesTypeFour } from '@/components/services/services-type-4';
+import { ServicesTypeSix } from '@/components/services/services-type-6';
 import { ServicesTypeSeven } from '@/components/services/services-type-7';
 
 export default function Home() {
@@ -22,6 +23,7 @@ export default function Home() {
       <ServicesTypeTwo data={SERVICES_SECTION_DATA} />
       <ServicesTypeTres data={SERVICES_SECTION_DATA} />
       <ServicesTypeFour data={SERVICES_SECTION_DATA} />
+      <ServicesTypeSix data={SERVICES_SECTION_DATA} />
       <ServicesTypeSeven data={SERVICES_SECTION_DATA} />
     </main>
   );
