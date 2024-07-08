433d5a0865c5734a20f6b167898b18a8137437c3
Bakhadir1988 <myblogfree1988@gmail.com>
2024-07-08 11:11:22 +0300

    add main template

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,18 @@
+import { AboutTypeOne } from '@/components/about';
+import { ABOUT_SECTION_DATA } from '@/components/about/mock';
+import { AdvantagesTypeOne } from '@/components/advantages';
+import { ADVANTAGES_SECTION_DATA } from '@/components/advantages/mock';
+import { ServicesTypeOne } from '@/components/services';
+import { SERVICES_SECTION_DATA } from '@/components/services/mock';
+import { StaffTypeOne } from '@/components/staff';
+
 export default function Home() {
   return (
-    <p>
-      Сюда импортируем компоненты согласно первому макету. Все компоненты
-      разложены по папкам пример (src/app/about/page)
-    </p>
+    <main>
+      <AdvantagesTypeOne data={ADVANTAGES_SECTION_DATA} />
+      <ServicesTypeOne data={SERVICES_SECTION_DATA} />
+      <AboutTypeOne data={ABOUT_SECTION_DATA} />
+      <StaffTypeOne />
+    </main>
   );
 }
--- src/lib/localdata.ts
+++ src/lib/localdata.ts
@@ -2,7 +2,7 @@ import fsPromises from 'fs/promises';
 import path from 'path';
 
 export async function getLocalData(url: string) {
-  await new Promise((resolve) => setTimeout(resolve, 3000));
+  await new Promise((resolve) => setTimeout(resolve, 5000));
   const filePath = path.join(process.cwd(), url);
   const jsonData = await fsPromises.readFile(filePath, 'utf-8');
   const data = JSON.parse(jsonData);
