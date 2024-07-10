972e3e9dd225c8469e5a8197e8b6d6f80bb265f7
Bakhadir1988 <myblogfree1988@gmail.com>
2024-07-11 02:24:07 +0300

    add slider component

--- src/shared/components/slider-ui/slider-ui.tsx
+++ src/shared/components/slider-ui/slider-ui.tsx
@@ -1,14 +1,20 @@
 'use client';
 
-import React from 'react';
+import clsx from 'clsx';
+import React, { ReactNode } from 'react';
 import 'swiper/css';
-import { Swiper } from 'swiper/react';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import { Navigation, Pagination } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
 
-// import styles from './slider-ui.module.scss';
+import { IconArrow } from '@/shared/icons/icon-arrow';
+
+import styles from './slider-ui.module.scss';
 
 export const SliderUi = ({
   children,
-  slidesPerView = 4,
+  slidesPerView = 1,
   spaceBetween = 20,
 }: {
   children?: React.ReactNode;
@@ -17,11 +23,59 @@ export const SliderUi = ({
 }) => {
   return (
     <Swiper
+      modules={[Navigation, Pagination]}
       spaceBetween={spaceBetween}
       slidesPerView={slidesPerView}
       loop={true}
+      className={styles.root}
+      pagination={{ el: '.navigation_line-pagination', clickable: true }}
+      navigation={{
+        nextEl: '.navigation_line-button-next',
+        prevEl: '.navigation_line-button-prev',
+      }}
+      breakpoints={{
+        320: {
+          slidesPerView: 1,
+        },
+        576: {
+          slidesPerView: 2,
+        },
+        991: {
+          slidesPerView: 3,
+        },
+      }}
     >
-      {children}
+      {React.Children.map(children, (child: ReactNode) => (
+        <SwiperSlide>{child}</SwiperSlide>
+      ))}
+
+      <div className={styles.navigation}>
+        <div className={styles.navigation_line}>
+          <div
+            className={clsx('navigation_line-pagination', styles.pagination)}
+          ></div>
+        </div>
+        <div className={styles.navigation_line}>
+          <button
+            className={clsx(
+              'navigation_line-button-prev',
+              styles.button,
+              styles.button_prev,
+            )}
+          >
+            <IconArrow width={16} height={16} fill="#1a57db" />
+          </button>
+          <button
+            className={clsx(
+              'navigation_line-button-next',
+              styles.button,
+              styles.button_next,
+            )}
+          >
+            <IconArrow width={16} height={16} fill="#1a57db" />
+          </button>
+        </div>
+      </div>
     </Swiper>
   );
 };
---
+++ src/shared/icons/icon-arrow.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+export const IconArrow = ({
+  width,
+  height,
+  fill,
+}: {
+  width?: number;
+  height?: number;
+  fill?: string;
+}) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={width}
+      height={height}
+      fill="none"
+    >
+      <path
+        fill={fill}
+        d="M16 7H3.83l5.59-5.59L8 0 0 8l8 8 1.41-1.41L3.83 9H16V7Z"
+      />
+    </svg>
+  );
+};
