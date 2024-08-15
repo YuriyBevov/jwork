160c43efa2bd21f09eb08c2f846bf08b371ee8d5
Bakhadir1988 <myblogfree1988@gmail.com>
2024-08-15 17:44:59 +0300

    add: footer-type-5

--- src/app/footer/page.tsx
+++ src/app/footer/page.tsx
@@ -1,22 +1,23 @@
 import {
-  // FooterType_1,
-  // FooterType_2,
-  // FooterType_3,
-  // FooterType_4,
-  FooterType_5, // FooterType_6,
-  // FooterType_7,
+  FooterType_1,
+  FooterType_2,
+  FooterType_3,
+  FooterType_4,
+  FooterType_5,
+  FooterType_6,
+  FooterType_7,
 } from '@/components/footer/components';
 
 export default function Footer() {
   return (
     <>
-      {/* <FooterType_1 />
+      <FooterType_1 />
       <FooterType_2 />
       <FooterType_3 />
-      <FooterType_4 /> */}
+      <FooterType_4 />
       <FooterType_5 />
-      {/* <FooterType_6 />
-      <FooterType_7 /> */}
+      <FooterType_6 />
+      <FooterType_7 />
     </>
   );
 }
