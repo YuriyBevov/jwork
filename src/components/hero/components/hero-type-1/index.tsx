be068066f91351a4eec5378faf961b8c08499e20
Bakhadir1988 <myblogfree1988@gmail.com>
2024-09-15 20:47:04 +0300

    hero type 1

--- src/components/hero/components/hero-type-1/index.tsx
+++ src/components/hero/components/hero-type-1/index.tsx
@@ -1,4 +1,4 @@
-import { Autocomplete, Select } from '@mantine/core';
+import { Button, MultiSelect, Select } from '@mantine/core';
 import React from 'react';
 
 import { HeaderType_1 } from '@/components/header/components';
@@ -7,8 +7,6 @@ import { Tab, TabContent, TabList } from '@/shared/components/tab';
 
 import { HeroDTO } from '../../types';
 import styles from '../hero.module.scss';
-
-i;
 
 export const HeroType_1 = async () => {
   const data: HeroDTO = await getLocalData(
@@ -29,50 +27,18 @@ export const HeroType_1 = async () => {
             <Tab className={styles.tab}>
               <TabList className={styles.tab_list}>
                 {data.tabs.map((tab) => (
-                  // <SegmentedControl
-                  //   data={
-                  //     key={tab.id}
-                  //     item.content &&
-                  //     item.content.map((contentItem) => ({
-                  //       label: contentItem.value,
-                  //       value: contentItem.value,
-                  //     }))
-                  //   }
-                  // />
                   <React.Fragment key={tab.id}>{tab.title}</React.Fragment>
                 ))}
               </TabList>
-              <TabContent className={styles.tab_content}>
-                {data.tabs.map((tab) => (
-                  <div key={tab.id}>
-                    {tab.items.map((item) =>
-                      item.type === 'select' ? (
-                        <Select
-                          className={styles.select}
-                          key={item.id}
-                          data={
-                            item.content &&
-                            item.content.map((contentItem) => ({
-                              label: contentItem.value,
-                              value: contentItem.value,
-                            }))
-                          }
-                          defaultValue={item.content?.[0].value}
-                          allowDeselect={false}
-                          comboboxProps={{
-                            transitionProps: {
-                              transition: 'pop',
-                              duration: 200,
-                            },
-                            shadow: 'md',
-                          }}
-                          // rightSection={}
-                        />
-                      ) : (
-                        <>
-                          <Autocomplete
-                            className={styles.select}
-                            placeholder={item.title}
+              <div>
+                <TabContent className={styles.tab_content}>
+                  {data.tabs.map((tab) => (
+                    <div key={tab.id}>
+                      {tab.items.map((item) =>
+                        item.type === 'multi_select' ? (
+                          <MultiSelect
+                            key={item.id}
+                            placeholder="Выбрать комнату"
                             data={
                               item.content &&
                               item.content.map((contentItem) => ({
@@ -80,14 +46,43 @@ export const HeroType_1 = async () => {
                                 value: contentItem.value,
                               }))
                             }
-                            limit={5}
+                            checkIconPosition="right"
                           />
-                        </>
-                      ),
-                    )}
-                  </div>
-                ))}
-              </TabContent>
+                        ) : (
+                          <>
+                            <Select
+                              className={styles.select}
+                              key={item.id}
+                              data={
+                                item.content &&
+                                item.content.map((contentItem) => ({
+                                  label: contentItem.value,
+                                  value: contentItem.value,
+                                }))
+                              }
+                              defaultValue={item.content?.[0].value}
+                              allowDeselect={false}
+                              comboboxProps={{
+                                transitionProps: {
+                                  transition: 'pop',
+                                  duration: 200,
+                                },
+                                shadow: 'md',
+                              }}
+                              searchable={item.type === 'search' ? true : false}
+                              // rightSection={}
+                            />
+                          </>
+                        ),
+                      )}
+                    </div>
+                  ))}
+                </TabContent>
+                <Button variant="outline">На карте</Button>
+                <Button radius="xl" color="green">
+                  Искать
+                </Button>
+              </div>
             </Tab>
           </div>
         </div>
