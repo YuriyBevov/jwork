7f9c4510856f9ddc6681a40a2ad916e96e6679a8
Bakhadir1988 <myblogfree1988@gmail.com>
2024-10-12 16:07:10 +0300

    hero template-1

--- src/components/hero/components/hero-type-1/index.tsx
+++ src/components/hero/components/hero-type-1/index.tsx
@@ -1,17 +1,14 @@
-import clsx from 'clsx';
 import React from 'react';
 
 import { HeaderType_1 } from '@/components/header/components';
+import { getLocalData } from '@/lib/localdata';
+import { HeroTab } from '@/shared/components/hero-tab';
 import {
   AutocompleteUi,
   MultiSelectUi,
   PriceRangeDropdownUi,
   SelectUi,
-} from '@/components/hero/ui';
-import { getLocalData } from '@/lib/localdata';
-import { ButtonUi } from '@/shared/components/button';
-import { Tab, TabContent, TabList } from '@/shared/components/tab';
-import { IconPin } from '@/shared/icons/icon-set-1/icon_pin';
+} from '@/shared/ui';
 
 import { HeroDTO, HeroItem } from '../../types';
 import styles from '../hero.module.scss';
@@ -26,11 +23,11 @@ export const HeroType_1 = async () => {
       case 'select':
         return <SelectUi className={styles.select} item={item} />;
       case 'multi_select':
-        return <MultiSelectUi className={styles.select} item={item} />;
+        return <MultiSelectUi item={item} />;
       case 'search':
-        return <AutocompleteUi className={styles.select} item={item} />;
+        return <AutocompleteUi item={item} />;
       case 'price_range':
-        return <PriceRangeDropdownUi className={styles.select} />;
+        return <PriceRangeDropdownUi />;
       default:
         return null;
     }
@@ -39,51 +36,7 @@ export const HeroType_1 = async () => {
   return (
     <>
       <HeaderType_1 />
-      <section className={clsx('container', styles.container)}>
-        <div
-          style={{ backgroundImage: 'url(' + data.backgroundImagePath + ')' }}
-          className={styles.root}
-        >
-          <div className={styles.content}>
-            <h1>{data.title}</h1>
-            <p>{data.description}</p>
-            <Tab className={styles.tab}>
-              <TabList className={styles.tab_list}>
-                {data.tabs.map((tab) => (
-                  <React.Fragment key={tab.id}>{tab.title}</React.Fragment>
-                ))}
-              </TabList>
-              <div className={styles.tab_content_wrapper}>
-                <TabContent className={styles.tab_content}>
-                  {data.tabs.map((tab) => (
-                    <div key={tab.id}>
-                      {tab.items.map((item) => (
-                        <React.Fragment key={item.id}>
-                          {selectItem(item)}
-                        </React.Fragment>
-                      ))}
-                    </div>
-                  ))}
-                </TabContent>
-
-                <ButtonUi
-                  icon={<IconPin fill="#1a57db" width={16} height={20} />}
-                  radius="xs"
-                  outline
-                  height="lg"
-                  style={{ backgroundColor: '#fff' }}
-                  className={styles.button}
-                >
-                  На карте
-                </ButtonUi>
-                <ButtonUi radius="xs" height="lg" className={styles.button}>
-                  Искать
-                </ButtonUi>
-              </div>
-            </Tab>
-          </div>
-        </div>
-      </section>
+      <HeroTab data={data} selectItem={selectItem} />
     </>
   );
 };
