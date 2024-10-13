78c7d99571b48c4cc4cc75b6428241b2b9528878
Bakhadir1988 <myblogfree1988@gmail.com>
2024-10-13 14:47:45 +0300

    add header component

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
 
-import { HeaderType_1, HeaderType_3 } from './components';
+import {
+  HeaderType_1,
+  HeaderType_3,
+  HeaderType_4,
+  HeaderType_6,
+} from './components';
 
-export const Header = ({ layout }: { layout: string }) => {
-  switch (layout) {
+export const Header = ({ templateID }: { templateID: string }) => {
+  switch (templateID) {
     case '1':
       return <HeaderType_1 />;
     case '2':
       return <HeaderType_3 />;
+    case '4':
+      return <HeaderType_4 />;
+    case '6':
+      return <HeaderType_6 />;
     default:
       return null;
   }
