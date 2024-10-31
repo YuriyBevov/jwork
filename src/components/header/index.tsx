b49a1b4c034b41501b7ce7a47a9905254ccb3191
Yuriy Bevov <yuriybevov@gmail.com>
2024-10-31 17:43:25 +0300

    Добавил шапки 4-7

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -3,8 +3,11 @@ import React from 'react';
 import {
   HeaderType_1,
   HeaderType_2,
-  HeaderType_3, // HeaderType_4,
-  // HeaderType_6,
+  HeaderType_3,
+  HeaderType_4,
+  HeaderType_5,
+  HeaderType_6,
+  HeaderType_7,
 } from './components';
 
 export const Header = ({ templateID }: { templateID: string }) => {
@@ -15,8 +18,14 @@ export const Header = ({ templateID }: { templateID: string }) => {
       return <HeaderType_2 />;
     case '3':
       return <HeaderType_3 />;
-    // case '6':
-    //   return <HeaderType_6 />;
+    case '4':
+      return <HeaderType_4 />;
+    case '5':
+      return <HeaderType_5 />;
+    case '6':
+      return <HeaderType_6 />;
+    case '7':
+      return <HeaderType_7 />;
     default:
       return null;
   }
