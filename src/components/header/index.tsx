8dfbc50dc08061cb1c5fedda17ca394dcc105ac3
Yuriy Bevov <yuriybevov@gmail.com>
2024-10-31 16:17:40 +0300

    Добавил хедер-3

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 import {
   HeaderType_1,
-  HeaderType_2, // HeaderType_3,
-  // HeaderType_4,
+  HeaderType_2,
+  HeaderType_3, // HeaderType_4,
   // HeaderType_6,
 } from './components';
 
@@ -13,8 +13,8 @@ export const Header = ({ templateID }: { templateID: string }) => {
       return <HeaderType_1 />;
     case '2':
       return <HeaderType_2 />;
-    // case '4':
-    //   return <HeaderType_4 />;
+    case '3':
+      return <HeaderType_3 />;
     // case '6':
     //   return <HeaderType_6 />;
     default:
--- src/shared/components/headerComponent/index.tsx
+++ src/shared/components/headerComponent/index.tsx
@@ -2,13 +2,13 @@
 
 import React from 'react';
 
-// export { HeaderType_3 } from './header-type-3/index';
 // export { HeaderType_4 } from './header-type-4/index';
 // export { HeaderType_6 } from './header-type-6/index';
 import { HeaderDTO } from '@/components/header/components/types';
 
 import { HeaderType_1 } from './components/header-type-1/index';
 import { HeaderType_2 } from './components/header-type-2/index';
+import { HeaderType_3 } from './components/header-type-3/index';
 
 export const HeaderComponent = ({
   data,
@@ -22,8 +22,8 @@ export const HeaderComponent = ({
       return <HeaderType_1 data={data} />;
     case '2':
       return <HeaderType_2 data={data} />;
-    // case '4':
-    //   return <HeaderType_4 />;
+    case '3':
+      return <HeaderType_3 data={data} />;
     // case '6':
     //   return <HeaderType_6 />;
     default:
