9686b83c9816572e7382e5b59e73cebe8d4769b4
Bakhadir1988 <myblogfree1988@gmail.com>
2024-11-05 21:45:28 +0300

    add: hero-5

--- src/components/hero/components/index.ts
+++ src/components/hero/components/index.ts
@@ -2,3 +2,4 @@ export { HeroType_1 } from './hero-type-1';
 export { HeroType_2 } from './hero-type-2';
 export { HeroType_3 } from './hero-type-3';
 export { HeroType_4 } from './hero-type-4';
+export { HeroType_5 } from './hero-type-5';
--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -2,7 +2,13 @@ import React from 'react';
 
 import { getLocalData } from '@/lib/localdata';
 
-import { HeroType_1, HeroType_2, HeroType_3, HeroType_4 } from './components';
+import {
+  HeroType_1,
+  HeroType_2,
+  HeroType_3,
+  HeroType_4,
+  HeroType_5,
+} from './components';
 import { HeroDTO } from './types';
 
 export const Hero = async ({ templateID }: { templateID: string }) => {
@@ -19,6 +25,8 @@ export const Hero = async ({ templateID }: { templateID: string }) => {
       return <HeroType_3 data={data} />;
     case '4':
       return <HeroType_4 data={data} />;
+    case '5':
+      return <HeroType_5 data={data} />;
     default:
       return null;
   }
