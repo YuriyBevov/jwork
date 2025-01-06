962d72c46281a87dbadadf94e6e6d357f3aa32dd
bakhadir <myblogfree1988@gmail.com>
2025-01-06 23:09:47 +0300

    Добавлен ключ API для Яндекс.Карт и иконка метки, обновлен компонент карты

--- src/components/yandex-map/index.tsx
+++ src/components/yandex-map/index.tsx
@@ -1,73 +1,63 @@
 'use client';
 
 import Script from 'next/script';
-import React from 'react';
 
 import style from './yandex-map.module.scss';
 
 export const YandexMap = () => {
+  const yandexUrl = `https://api-maps.yandex.ru/2.1/?apikey=${process.env.NEXT_PUBLIC_YANDEX_MAP_KEY}&lang=ru_RU`;
+
+  const placemarks = [
+    {
+      coordinates: [55.661574, 37.573856],
+      hintContent: 'Собственный значок метки с контентом',
+      balloonContent: 'А эта — новогодняя',
+      iconContent: '1456',
+    },
+    {
+      coordinates: [55.751574, 37.573856],
+      hintContent: 'Вторая метка',
+      balloonContent: 'Это вторая метка',
+      iconContent: '2',
+    },
+  ];
+
   return (
     <>
       <div className={style.root} id="map"></div>
 
       <Script
-        src="https://api-maps.yandex.ru/2.1/?apikey=43d53367-d7a5-4974-bf78-21f39213e203&lang=ru_RU"
+        src={yandexUrl}
         onReady={() => {
           const ymaps = window.ymaps;
           ymaps.ready(function () {
             const myMap = new ymaps.Map('map', {
                 center: [55.751574, 37.573856],
-                zoom: 9,
+                zoom: 8,
               }),
-              // Создаём макет содержимого.
               MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
-                '<div style="color: #000; font-weight: bold;">$[properties.iconContent]</div>',
-              ),
-              myPlacemark = new ymaps.Placemark(
-                myMap.getCenter(),
+                '<div style="display: flex; align-items: center; justify-content: center; width: 80px; height: 80px;font-size: 24px; color: #1A56DB; font-weight: bold;">$[properties.iconContent]</div>',
+              );
+
+            placemarks.forEach((placemarkData) => {
+              const placemark = new ymaps.Placemark(
+                placemarkData.coordinates,
                 {
-                  hintContent: 'Собственный значок метки',
-                  balloonContent: 'Это красивая метка',
+                  hintContent: placemarkData.hintContent,
+                  balloonContent: placemarkData.balloonContent,
+                  iconContent: placemarkData.iconContent,
                 },
                 {
-                  // Опции.
-                  // Необходимо указать данный тип макета.
-                  iconLayout: 'default#image',
-                  // Своё изображение иконки метки.
-                  iconImageHref: 'images/myIcon.gif',
-                  // Размеры метки.
-                  iconImageSize: [30, 42],
-                  // Смещение левого верхнего угла иконки относительно
-                  // её "ножки" (точки привязки).
-                  iconImageOffset: [-5, -38],
-                },
-              ),
-              myPlacemarkWithContent = new ymaps.Placemark(
-                [55.661574, 37.573856],
-                {
-                  hintContent: 'Собственный значок метки с контентом',
-                  balloonContent: 'А эта — новогодняя',
-                  iconContent: '12',
-                },
-                {
-                  // Опции.
-                  // Необходимо указать данный тип макета.
                   iconLayout: 'default#imageWithContent',
-                  // Своё изображение иконки метки.
-                  iconImageHref: 'images/ball.png',
-                  // Размеры метки.
-                  iconImageSize: [48, 48],
-                  // Смещение левого верхнего угла иконки относительно
-                  // её "ножки" (точки привязки).
-                  iconImageOffset: [-24, -24],
-                  // Смещение слоя с содержимым относительно слоя с картинкой.
-                  iconContentOffset: [15, 15],
-                  // Макет содержимого.
+                  iconImageHref: '/assets/images/placemark.svg',
+                  iconImageSize: [80, 80],
+                  iconImageOffset: [0, 0],
+                  iconContentOffset: [0, 0],
                   iconContentLayout: MyIconContentLayout,
                 },
               );
-
-            myMap.geoObjects.add(myPlacemark).add(myPlacemarkWithContent);
+              myMap.geoObjects.add(placemark);
+            });
           });
         }}
       />
